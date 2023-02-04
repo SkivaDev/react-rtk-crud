b5165c6eca8641c71aa66b57c49a93dc19167520
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-02-03 23:56:54 -0500

    List and Create part 1

--- src/components/task-form/TaskForm.jsx
+++ src/components/task-form/TaskForm.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TaskForm() {
+
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  })
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(task);
+  }
+
   return (
-    <div>TaskForm</div>
+    <form onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='title' onChange={handleChange}/>
+
+      <textarea name="description" placeholder='description' onChange={handleChange}></textarea>
+
+      <button>Save</button>
+    </form>
   )
 }
 
