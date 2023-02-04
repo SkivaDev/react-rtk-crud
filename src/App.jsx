07db6cdae1ae1f534e704d2a8adedd47f959d21f
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-02-03 21:43:42 -0500

    Redux Toolkit added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+// import './App.css'
+import { useSelector } from 'react-redux'
+import TaskForm from './components/task-form/TaskForm';
+import TaskList from './components/task-list/TaskList';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const tasksState = useSelector(state => state.tasks);
 
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <TaskForm/>
+      <TaskList/>
     </div>
   )
 }
